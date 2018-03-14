07d6f5c9db636ece12ca3c4e6cadcb35b37cc68c
mrwe <erikwik@stud.ntnu.no>
2018-03-14 23:17:53 +0100

    Now with same styling as rules

--- src/main.js
+++ src/main.js
@@ -1,34 +1,41 @@
+let counter = 0;
+
 if (location.href.includes("online.ntnu.no/events")) {
-  var interval = setInterval(setNames, 100);
+  var interval = setInterval(setNames, 50);
+}
+
+function showMembers(){
+    $('.event-details-single-image').css("background-color", "#d6eaff");
+
+    $('.event-details-single-image').prepend("<div class='col-md-12'> </div>");
+    $('.event-details-single-image > .col-md-12').prepend("<div class='event-access'>  </div>");
+    $('.event-details-single-image > .col-md-12 > .event-access').append("<p class='access-title'> Medlemmer som skal </p>");
+    $('.event-details-single-image > .col-md-12 > .event-access').append("<div class='rules'> </div>");
+
+    $('#attendee_list > tr').each((x, y) => {
+      $('.event-details-single-image > .col-md-12 > .event-access > .rules').append('<span class="label label-info wiker">' + $(y).text().replace(/ /g,' ') + '</span>');
+    });
+
+
+    $('.rules > .wiker').css({
+      'word-wrap': 'normal',
+      'display': 'inline-block',
+      'white-space': '',
+      'margin': '1px 2px',
+      'line-height': '13px'
+    });
 }
 
 function setNames() {
 
-  let attendee_list = $('#attendee_list');
+  let attendee_list = $('#attendee_list > tr');
 
-
-  if (attendee_list != "") {
-
-    $('.event-details-single-image').prepend('<td></td>')
-
-    $('.event-details-single-image').prepend("<p>Medlemmer som skal </p>");
-
-    $('#attendee_list > tr').each((x, y) =>{
-      $('.event-details-single-image > td').prepend(y)
-    });
-
-
-    $('.event-details-single-image > p').css({
-    'color': '#ee7810',
-    'font-size': '15px',
-    'font-weight': '300',
-    'text-align': 'center'});
-
-    $('.event-details-single-image').css( "background-color", "#d6eaff" );
-    $('.event-details-single-image > td').css({
-      "padding": "5px"});
-
-
+  if (attendee_list.length != 0) {
+    showMembers();
     clearInterval(interval);
   }
+  else if(counter > 50){
+    clearInterval(interval);
+  }
+  counter += 1
 }
